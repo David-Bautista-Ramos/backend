2f7c5086b02ffe436535c175579b42ef6abe9c77
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-11-19 08:40:40 -0500

    first commit

--- middleware/protectRoutes.js
+++ middleware/protectRoutes.js
@@ -1,30 +1,28 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 import jwt from "jsonwebtoken";
 
-export const protectRoutes = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
-    if (!token) {
-      return res
-        .status(401)
-        .json({ error: "Sin Autorización: No proporiona el Token" });
+export const protectRoutes = async (req, res, next)=>{
+    try {
+        const token = req.cookies.jwt;
+        if(!token) {
+            return res.status(401).json({error: "Sin Autorización: No proporiona el Token"});
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded) {
+            return res.status(401).json({error:"Sin Autorización:Token invalido"})
+        }
+
+        const user =await User.findById(decoded.userId).select("-contrasena");
+
+        if(!user){
+            return res.status(404).json({error:"Usuario no encontrado"})
+        }
+
+        req.user=user;
+        next();
+    } catch (error) {
+        console.log("Error in protectRoutes middleware",error.message);
+        return res.status(500).json({error:"Error server interno"})
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (!decoded) {
-      return res.status(401).json({ error: "Sin Autorización:Token invalido" });
-    }
-
-    const user = await User.findById(decoded.userId).select("-contrasena");
-
-    if (!user) {
-      return res.status(404).json({ error: "Usuario no encontrado" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log("Error in protectRoutes middleware", error.message);
-    return res.status(500).json({ error: "Error server interno" });
-  }
-};
+}
